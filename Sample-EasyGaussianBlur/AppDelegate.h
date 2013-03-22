c87f150c2a8f64e21d7193c723b4667b0cd74a63
STatsu <STatsu@USER-no-MacBook-Pro-7.local>
2013-03-22 11:03:36 +0900

    Initial Commit

---
+++ Sample-EasyGaussianBlur/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sample-EasyGaussianBlur
+//
+//  Created by STatsu on 13/03/22.
+//  Copyright (c) 2013年 objc-Lovers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sample-EasyGaussianBlur/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sample-EasyGaussianBlur
+//
+//  Created by STatsu on 13/03/22.
+//  Copyright (c) 2013年 objc-Lovers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
