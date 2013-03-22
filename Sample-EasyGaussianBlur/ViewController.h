245d6d899a50b91f514354cf61a6445545e5c452
STatsu <STatsu@USER-no-MacBook-Pro-7.local>
2013-03-22 11:31:50 +0900

    Update 2013/03/22

---
+++ Sample-EasyGaussianBlur/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sample-EasyGaussianBlur
+//
+//  Created by STatsu on 13/03/22.
+//  Copyright (c) 2013年 objc-Lovers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Sample-EasyGaussianBlur/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  Sample-EasyGaussianBlur
+//
+//  Created by STatsu on 13/03/22.
+//  Copyright (c) 2013年 objc-Lovers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
